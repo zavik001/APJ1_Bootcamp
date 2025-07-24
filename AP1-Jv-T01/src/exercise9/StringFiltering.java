bbda0f9c04aa6a9eab67826c88a70f7f93fbf967
Zavik <zav021104@gmail.com>
2025-07-25 00:57:45 +0700

    fear: add sol for ex9

---
+++ AP1-Jv-T01/src/exercise9/StringFiltering.java
@@ -0,0 +1,59 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringFiltering {
+
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<String> lines = new ArrayList<String>();
+        String filter = null;
+        try {
+            Integer t = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < t; i++) {
+                lines.add(reader.readLine());
+            }
+            filter = reader.readLine();
+            reader.close();
+        } catch (IOException e) {
+        }
+
+        boolean isFirst = true;
+        for (String line : lines) {
+            if (isUnderLine(filter, line)) {
+                if (isFirst) {
+                    System.out.println(line);
+                } else {
+                    System.out.println(", " + line);
+                }
+            }
+        }
+    }
+
+    public static boolean isUnderLine(String underLine, String line) {
+        int i = 0;
+        boolean flag = false;
+        if (underLine == null || line == null) {
+            return false;
+        }
+        if (underLine.length() > line.length()) {
+            return false;
+        }
+        for (char c : line.toCharArray()) {
+            if (c == underLine.charAt(i)) {
+                flag = true;
+                i++;
+                if (i == underLine.length()) {
+                    break;
+                }
+            } else {
+                flag = false;
+                i = 0;
+            }
+        }
+        return flag;
+    }
+}
